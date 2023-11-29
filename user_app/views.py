53001f15b4132d196a829581152cecb517ef08d8
BGabor01 <busnes14@gmail.com>
2023-11-29 21:35:37 +0100

    user_app.views: Added CreateUserView

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers.registration_serializer import RegistrationSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = RegistrationSerializer

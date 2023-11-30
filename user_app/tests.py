1d63c35a88f49fb54daefe0bc27cdfe87f2f701f
BGabor01 <busnes14@gmail.com>
2023-11-30 18:55:18 +0100

    tests for basic auth

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserAppTest(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='testloginuser', password='asdasd123',
+                                 first_name='test', last_name='login', email='test@login.com')
+
+    def test_registration_success(self):
+        response = self.client.post(reverse('create_user'), data={
+            'username': 'testuser',
+            'password': 'complex_password',
+            'first_name': 'test',
+            'last_name': 'user',
+            'email': 'test@user.com'
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_registration_fail(self):
+        response = self.client.post(reverse('create_user'), data={
+            'username': 'testuser2',
+            'password': 'complex_password',
+            'last_name': 'user',
+            'email': 'test@user.com'
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_login_success(self):
+        response = self.client.post(reverse('login'), data={
+            'username': 'testloginuser',
+            'password': 'asdasd123',
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_fail(self):
+        response = self.client.post(reverse('login'), data={
+            'username': 'testloginuser2',
+            'password': 'mjfhzeiw',
+        })
+        self.assertEqual(response.status_code, 401)

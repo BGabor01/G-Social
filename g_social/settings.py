7a559476aec4ab7bb2005f8a29054ead3926a3c8
BGabor01 <busnes14@gmail.com>
2023-12-01 17:00:50 +0100

    settings: Added media settings

--- g_social/settings.py
+++ g_social/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -143,3 +144,7 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'

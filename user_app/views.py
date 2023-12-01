6a056948185fa535292967e462dc4b40b20206e3
BGabor01 <busnes14@gmail.com>
2023-12-01 17:36:02 +0100

    user_app.view: Fixed UpdateProfileView

--- user_app/views.py
+++ user_app/views.py
@@ -4,6 +4,7 @@ from rest_framework import permissions
 from .serializers.registration_serializer import RegistrationSerializer
 from .serializers.update_user_profile_serializer import UserProfileUpdateSerializer
 from user_app.permissions.IsOwnerPermission import IsOwner
+from user_app.models import UserProfileModel
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -11,5 +12,6 @@ class CreateUserView(generics.CreateAPIView):
 
 
 class UpdateProfileView(generics.UpdateAPIView):
-    authentication_classes = [permissions.IsAuthenticated, IsOwner]
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
     serializer_class = UserProfileUpdateSerializer
+    queryset = UserProfileModel.objects.all()

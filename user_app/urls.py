43f7618d72983cedbd6ce0a33d940f7d1881c143
BGabor01 <busnes14@gmail.com>
2023-12-01 23:03:48 +0100

    user_app.urls: Added retrieve-user path

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,12 +4,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from user_app.views import CreateUserView, UpdateProfileView
+from user_app.views import CreateUserView, UpdateProfileView, RetrieveUserDataView
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create_user'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshView.as_view(), name='login_refresh'),
     path('update-profile/<int:pk>/',
-         UpdateProfileView.as_view(), name='update-profile')
+         UpdateProfileView.as_view(), name='update-profile'),
+    path('retrieve-user/<int:pk>/',
+         RetrieveUserDataView.as_view(), name="retrieve_user")
 ]

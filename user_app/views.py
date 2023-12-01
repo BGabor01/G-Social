11ff02c7609be1e3f9cab4faee5522553e8e6b17
BGabor01 <busnes14@gmail.com>
2023-12-01 17:18:28 +0100

    user_app.views: created UpdateProfileView

--- user_app/views.py
+++ user_app/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
+from rest_framework import permissions
 
 from .serializers.registration_serializer import RegistrationSerializer
+from .serializers.update_user_profile_serializer import UserProfileUpdateSerializer
+from user_app.permissions.IsOwnerPermission import IsOwner
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = RegistrationSerializer
+
+
+class UpdateProfileView(generics.UpdateAPIView):
+    authentication_classes = [permissions.IsAuthenticated, IsOwner]
+    serializer_class = UserProfileUpdateSerializer

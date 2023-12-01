86442393a304af74b2d00ebec3c992e762ffb2c4
BGabor01 <busnes14@gmail.com>
2023-12-01 17:54:04 +0100

    user_app.models: Fixed default picture path

--- user_app/models.py
+++ user_app/models.py
@@ -6,7 +6,7 @@ class UserProfileModel(models.Model):
     owner = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="profile_data")
     profile_picture = models.ImageField(
-        default="default.jpg", upload_to="profile_pics")
+        default="profile_pics/default.jpg", upload_to="profile_pics")
 
     def __str__(self) -> str:
         return f"{self.owner.username}'s profile"

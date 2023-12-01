bf68dae3b1a9c563a30a03ae541bff94a4a549ad
BGabor01 <busnes14@gmail.com>
2023-12-01 17:37:28 +0100

    user_app.urls: Adde pk to update-profile path

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,11 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import CreateUserView, UpdateProfileView
+from user_app.views import CreateUserView, UpdateProfileView
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create_user'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshView.as_view(), name='login_refresh'),
-    path('update-profile/', UpdateProfileView.as_view(), name='update-profile')
+    path('update-profile/<int:pk>/',
+         UpdateProfileView.as_view(), name='update-profile')
 ]

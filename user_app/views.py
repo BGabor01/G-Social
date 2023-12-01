32a4e651855610ae920a29840cc468089fdfcaa6
BGabor01 <busnes14@gmail.com>
2023-12-01 23:04:06 +0100

    user_app.views: Created RetrieveUserDataView

--- user_app/views.py
+++ user_app/views.py
@@ -1,14 +1,19 @@
 from rest_framework import generics
 from rest_framework import permissions
+from django.contrib.auth.models import User
 
-from .serializers.registration_serializer import RegistrationSerializer
-from .serializers.update_user_profile_serializer import UserProfileUpdateSerializer
+from user_app.serializers import RegistrationSerializer, UserDataSerializer, UserProfileUpdateSerializer
 from user_app.permissions.IsOwnerPermission import IsOwner
 from user_app.models import UserProfileModel
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = RegistrationSerializer
+
+
+class RetrieveUserDataView(generics.RetrieveAPIView):
+    serializer_class = UserDataSerializer
+    queryset = User.objects.all()
 
 
 class UpdateProfileView(generics.UpdateAPIView):

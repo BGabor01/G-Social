0af223f403413a1ca4a484e63cb1f24a0a535e6a
BGabor01 <busnes14@gmail.com>
2023-12-02 13:55:23 +0100

    user_app.user_created_signal: Sendign request to email sender service

--- user_app/signals/user_created_signal.py
+++ user_app/signals/user_created_signal.py
@@ -3,9 +3,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from user_app.models import UserProfileModel
+from user_app.rpc import welcome_rpc_client
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         UserProfileModel.objects.create(owner=instance)
+        welcome_rpc_client.send_request("registration", instance.email)

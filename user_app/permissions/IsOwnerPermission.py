ec3099b628d3c2ec9deb623c4d1441029b683bdc
BGabor01 <busnes14@gmail.com>
2023-12-02 14:22:35 +0100

    user_app: Added docstrings and type hints

--- user_app/permissions/IsOwnerPermission.py
+++ user_app/permissions/IsOwnerPermission.py
@@ -1,6 +1,35 @@
+from django.http import HttpRequest
 from rest_framework import permissions
+from rest_framework import generics
+from django.db import models
 
 
 class IsOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    """
+    Custom permission to only allow owners of an object to edit it.
+
+    This class extends the BasePermission class from Django REST framework's
+    permissions module. It overrides the has_object_permission method to check
+    whether the user making the request is the 'owner' of the object.
+
+    Methods:
+        - has_object_permission: Checks if the request user is the owner of the object.
+
+    Usage:
+        - This permission can be applied to views by including it in the view's
+        'permission_classes' list.
+    """
+
+    def has_object_permission(self, request: HttpRequest, view: generics.GenericAPIView, obj: models.Model) -> bool:
+        """
+        Check if the user making the request is the owner of the object.
+
+        Parameters:
+            - request (HttpRequest): The HTTP request from the user.
+            - view (GenericAPIView): The view which this permission is applied to.
+            - obj (Model instance): The object that is being accessed or manipulated.
+
+        Returns:
+            bool: True if the request user is the owner of the object, False otherwise.
+        """
         return obj.owner == request.user
--- user_app/serializers/profile_serializers.py
+++ user_app/serializers/profile_serializers.py
@@ -4,6 +4,22 @@ from user_app.models import UserProfileModel
 
 
 class UserProfileUpdateSerializer(serializers.ModelSerializer):
+    """
+    Serializer for updating a user's profile picture.
+
+    This serializer is specifically designed for the UserProfileModel, targeting
+    the update of the profile picture.
+
+    Fields:
+        - profile_picture (ImageField): An ImageField required for updating the 
+                                      profile picture of the user. It must be 
+                                      provided when using this serializer.
+
+    Meta:
+        - model (Model): The UserProfileModel which this serializer is associated with.
+        - fields (list): List containing 'profile_picture' to specify the only field
+                       this serializer handles.
+    """
     profile_picture = serializers.ImageField(required=True)
 
     class Meta:
@@ -12,6 +28,14 @@ class UserProfileUpdateSerializer(serializers.ModelSerializer):
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the complete user profile data.
+
+    Meta:
+        - model (Model): The UserProfileModel which this serializer is associated with.
+        - fields (str): '__all__' indicates that all fields of UserProfileModel are to be
+                      included in this serializer.
+    """
     class Meta:
         model = UserProfileModel
         fields = '__all__'

79cc3ebbe0e2ab5f4bb4de9a54b34d29fbf1e624
BGabor01 <busnes14@gmail.com>
2023-12-29 23:36:04 +0100

    wsgi: default env

--- g_social/wsgi.py
+++ g_social/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'g_social.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          f'g_social.settings.settings_{os.environ.get("ENVIRONMENT")}')
 
 application = get_wsgi_application()

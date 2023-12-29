03dd34a23a78dc9ae1263fb17aded0194fd705fb
BGabor01 <busnes14@gmail.com>
2023-12-29 23:36:20 +0100

    gunicorn.conf: gunicorn config

---
+++ g_social/gunicorn.conf.py
@@ -0,0 +1,11 @@
+reload = False
+
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+limit_request_line = 4094
+limit_request_fields = 20
+limit_request_field_size = 8190
+
+proc_name = 'g_social_app'

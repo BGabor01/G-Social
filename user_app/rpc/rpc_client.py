421ac6f51c5ec4d40af932825d7b234d6bb8b03e
BGabor01 <busnes14@gmail.com>
2023-12-29 23:35:32 +0100

    rpc_client: temporary solution

--- user_app/rpc/rpc_client.py
+++ user_app/rpc/rpc_client.py
@@ -1,3 +1,10 @@
+import os
+import time
 from g_rpc import Client
 
-welcome_rpc_client = Client("example_service")
+try:
+    time.sleep(10)
+    welcome_rpc_client = Client("EMAIL_SENDER", host=os.environ.get("RABBITMQ_HOST"), username=os.environ.get("RABBITMQ_USER"), password=os.environ.get("RABBITMQ_PASS"))
+except Exception as e:
+    print("Rabbit not ready yet")
+

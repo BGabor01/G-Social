42ef2cd220714c355a80b977df3791229f347eb2
BGabor01 <busnes14@gmail.com>
2023-12-30 22:09:51 +0100

    rpc_client: Default values

--- user_app/rpc/rpc_client.py
+++ user_app/rpc/rpc_client.py
@@ -2,5 +2,5 @@ import os
 import time
 from g_rpc import Client
 
-welcome_rpc_client = Client("EMAIL_SENDER", host=os.environ.get("RABBITMQ_HOST"), username=os.environ.get("RABBITMQ_USER"), password=os.environ.get("RABBITMQ_PASS"))
+welcome_rpc_client = Client("EMAIL_SENDER", host=os.environ.get("RABBITMQ_HOST", "localhost"), username=os.environ.get("RABBITMQ_USER","guest"), password=os.environ.get("RABBITMQ_PASS","guest"))
 

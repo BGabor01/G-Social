63075fba2e3891fe857a1ed10acdf84c6e006f4e
BGabor01 <busnes14@gmail.com>
2023-12-30 18:16:58 +0100

    setting_prod: allowed host and foward host

--- g_social/settings/settings_prod.py
+++ g_social/settings/settings_prod.py
@@ -1,5 +1,9 @@
 from g_social.settings.settings import *
 
 SECRET_KEY = os.environ.get("PROD_SECRET_KEY")
-ALLOWED_HOSTS = ['django']
+
+ALLOWED_HOSTS = ['django','127.0.0.1','localhost']
+
+USE_X_FORWARDED_HOST = True
+
 DEBUG = False

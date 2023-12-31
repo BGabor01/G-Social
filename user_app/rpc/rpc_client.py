5e25346a124aec444311ce2f6787b2337a22912f
BGabor01 <busnes14@gmail.com>
2023-12-31 14:40:49 +0100

    rpc_client: Deleted unused import

--- user_app/rpc/rpc_client.py
+++ user_app/rpc/rpc_client.py
@@ -1,5 +1,5 @@
 import os
-import time
+
 from g_rpc import Client
 
 welcome_rpc_client = Client("EMAIL_SENDER", host=os.environ.get("RABBITMQ_HOST", "localhost"), username=os.environ.get("RABBITMQ_USER","guest"), password=os.environ.get("RABBITMQ_PASS","guest"))

e5f8fc4484bc69a423706508ba84011a261926f2
BGabor01 <busnes14@gmail.com>
2024-01-05 21:30:39 +0100

    user_app: no need rpc client

--- user_app/rpc/rpc_client.py
+++
@@ -1,6 +0,0 @@
-import os
-
-from g_rpc import Client
-
-welcome_rpc_client = Client("EMAIL_SENDER", host=os.environ.get("RABBITMQ_HOST", "localhost"), username=os.environ.get("RABBITMQ_USER","guest"), password=os.environ.get("RABBITMQ_PASS","guest"))
-

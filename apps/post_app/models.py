8945ca14dfbb6acc3646eb7d7c1bb59ffe7a6bae
BGabor01 <busnes14@gmail.com>
2024-01-06 22:48:20 +0100

    PostModel: Added created_at field

--- apps/post_app/models.py
+++ apps/post_app/models.py
@@ -5,6 +5,7 @@ class PostModel(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=255, blank=True, null=True)
     image = models.ImageField(blank=True, null=True, upload_to="user_pics")
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     def __str__(self) -> str:
         return f"{self.owner.username}'s post"

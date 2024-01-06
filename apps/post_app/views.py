d0e68b9bc489e43980f5cb46dbe5e9dd3cf009ce
BGabor01 <busnes14@gmail.com>
2024-01-06 22:50:09 +0100

    post_app.views: Create -> ListCreateApiView, pagination

--- apps/post_app/views.py
+++ apps/post_app/views.py
@@ -3,12 +3,16 @@ from rest_framework.permissions import IsAuthenticated
 
 from apps.post_app.serializers import PostSerializer
 from apps.post_app.models import PostModel
+from apps.post_app.paginations import PostCursorPagination
 from apps.components.permissions import IsOwner
 
-class CreatePostView(generics.CreateAPIView):
+
+class ListOrCreatePostView(generics.ListCreateAPIView):
 
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
+    pagination_class = PostCursorPagination
+    queryset = PostModel.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)

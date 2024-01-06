7142891289e3e32e6b0c3486dfbee5400306f3fa
BGabor01 <busnes14@gmail.com>
2024-01-06 21:49:50 +0100

    urls: Added post app ulrs

--- g_social/urls.py
+++ g_social/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include("apps.user_app.urls")),
+    path('api/user/', include('apps.user_app.urls')),
+    path('api/post/', include('apps.post_app.urls')),
     path('api/health/', include('health_check.urls'))
 ]
 if settings.DEBUG:

794196a5214b0fd2a6b935b15bc5e7ea2e0799da
BGabor01 <busnes14@gmail.com>
2024-01-14 21:54:20 +0100

    admin site: Registered models

--- apps/friend_app/admin.py
+++ apps/friend_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.friend_app.models import FriendListModel, FriendRequestModel
 
 # Register your models here.
+
+admin.site.register(FriendListModel)
+admin.site.register(FriendRequestModel)
